ba633d6939eb7d88bf25b5fe74233ca7e0339a5c
SergioGaon <serxav13@gmail.com>
2021-04-17 23:13:46 -0400

    Prueba 9 grilla

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,20 +77,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'root$blogbd>',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
-#}
 
 
 # Password validation
